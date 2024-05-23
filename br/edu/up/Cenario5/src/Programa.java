dcde77341be759765d2b6555d2a34b08bac61233
yohaa22 <145264388+yohaa22@users.noreply.github.com>
2024-05-23 19:44:31 -0300

    ex 05.2

--- br/edu/up/Cenario5/src/Programa.java
+++ br/edu/up/Cenario5/src/Programa.java
@@ -1,9 +1,10 @@
 
 
-package up.Cenario5.src.view;
+package up.Cenario5.src;
 
 import up.Cenario5.src.controls.ControladorEvento;
 import up.Cenario5.src.controls.ControladorReserva;
+import up.Cenario5.src.view.View;
 
 public class Programa {
     public static void main(String[] args) {
